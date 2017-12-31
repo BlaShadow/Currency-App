ec9fa7f1f561019854b6918892ae2dbdc2f9b62f
blashadow <blackzerogamer@gmail.com>
2017-12-31 15:34:43 -0400

    dark status bar.

--- app/components/LocalStatusBar/LocalStatusBar.js
+++ app/components/LocalStatusBar/LocalStatusBar.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 
+const DarkStatusBar = () => <StatusBar translucent={true} barStyle="dark-content" />
+
 export default () => <StatusBar translucent={false} barStyle="light-content" />
+
+export { DarkStatusBar };
