7aabac2092f0d11b5257f9293d4246d8aa555c9b
Luis Romero <Luis_Romero@claro.com.do>
2018-01-10 18:11:45 -0400

    App with redux provider

--- app/index.js
+++ app/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import { Provider } from 'react-redux';
 
 import Home from './screens/Home';
 import CurrencyList from './screens/CurrencyList';
 import Options from './screens/Options';
 import ThemeOptions from './screens/ThemeOptions';
-
+import store from './config/store';
 import StackNavigator from './config/route';
 
 EStyleSheet.build({
     $primaryBlue: '#03A9F4', 
 });
 
-export default () => <StackNavigator />;
+export default () => (
+    <Provider store={store}>
+        <StackNavigator />
+    </Provider>
+);
