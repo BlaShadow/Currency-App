1e4415aaabfdee38986c1cd6fe55b2f9600c76bf
blashadow <blackzerogamer@gmail.com>
2018-01-13 16:05:55 -0400

    set currency actions.

--- app/actions/currencies.js
+++ app/actions/currencies.js
@@ -1,5 +1,7 @@
 export const CHANGE_CURRENCY_AMOUNT = 'CHANGE_CURRENCY_AMOUNT';
 export const SWAP_CURRENCY = 'SWAP_CURRENCY';
+export const SET_BASE_CURRENCY = 'SET_BASE_CURRENCY';
+export const SET_QUOTE_CURRENCY = 'SET_QUOTE_CURRENCY';
 
 export const changeCurrencyAmount = amount => ({
     type: CHANGE_CURRENCY_AMOUNT,
@@ -8,4 +10,14 @@ export const changeCurrencyAmount = amount => ({
 
 export const swapCurrency = () => ({
     type: SWAP_CURRENCY
+});
+
+export const setBaseCurrency = (baseCurrency) => ({
+    type: SET_BASE_CURRENCY,
+    baseCurrency
+});
+
+export const setQuoteCurrency = (quoteCurrency) => ({
+    type: SET_QUOTE_CURRENCY,
+    quoteCurrency
 });
